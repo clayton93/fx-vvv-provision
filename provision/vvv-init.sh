c8790ebeecfd99763133f86dc43f5ab37136b40c
Clayton Jones <clayton@wearefx.co.uk>
2017-03-28 11:31:53 +0100

    Moved files into the right folder; fixed sql error

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,10 +1,12 @@
-dd the site name to the hosts file
+#!/usr/bin/env bash
+
+# Add the site name to the hosts file
 echo "127.0.0.1 ${VVV_SITE_NAME}.local # vvv-auto" >> "/etc/hosts"
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${VVV_SITE_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS `${VVV_SITE_NAME}`"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON `${VVV_SITE_NAME}`.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e 'CREATE DATABASE IF NOT EXISTS `${VVV_SITE_NAME}`;'
+mysql -u root --password=root -e 'GRANT ALL PRIVILEGES ON `${VVV_SITE_NAME}`.* TO wp@localhost IDENTIFIED BY 'wp';'
 echo -e "\n DB operations done.\n\n"
 
 # Nginx Logs
